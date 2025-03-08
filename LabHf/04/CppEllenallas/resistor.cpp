52953643c688388209772e501c76754a0a1b0e19
Hgz05 <hejagergo@gmail.com>
2025-03-08 18:29:20 +0100

    asd

--- LabHf/04/CppEllenallas/resistor.cpp
+++ LabHf/04/CppEllenallas/resistor.cpp
@@ -12,3 +12,39 @@
  */
 
 #include "resistor.h"
+
+double Resistor::defR = 72;
+Resistor::Resistor()
+{
+    this->R = defR;
+}
+
+Resistor::Resistor(double r)
+{
+    this->R = r;
+}
+
+void Resistor::setDef(double r)
+{
+    defR = r;
+}
+
+Resistor Resistor::operator+(const Resistor &r) const
+{
+    return Resistor(this->R + r.R);
+}
+
+Resistor Resistor::operator%(const Resistor &r) const
+{
+    double a = (1 / this->R) + (1 / r.R);
+    return Resistor(1 / a);
+}
+
+Resistor operator*(int n, const Resistor &r)
+{
+    if (n <= 0)
+    {
+        throw "FEFSRB";
+    }
+    return Resistor(r.getR() * n);
+}
